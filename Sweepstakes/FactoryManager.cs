dd7850c9780c171d4af7393f3bfedd2081f12cd6
Jake Gambino <jake.gambino1@gmail.com>
2019-09-18 14:46:41 -0500

    added factory design for choosing sweepstakes management type.

---
+++ Sweepstakes/FactoryManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sweepstakes
+{
+    class FactoryManager
+    {
+        public ISweepstakesManager ChooseSweepstakesManagement()
+        {
+            string userInput;
+            userInput = UI.GetUserStringInput("Would you like this to be in a queue 'q' or a 'stack'?");
+
+            if (userInput == "q" || userInput == "s")
+            {
+                switch (userInput)
+                {
+                    case "q":
+                        return new SweepstakesQueueManager();
+                    case "s":
+                        return new SweepstakesStackManager();
+                }
+            }
+            Console.WriteLine("Please make a valid choice");
+            return ChooseSweepstakesManagement();
+        }
+    }
+}
